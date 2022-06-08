c26383be171039534418111ababadbc8059e1ab5
EC2 Default User <ec2-user@ip-172-31-79-189.ec2.internal>
2022-06-08 12:36:41 +0000

    Initial Commit

---
+++ Ruby/include.rb
@@ -0,0 +1,1 @@
+puts "WEBCAMPでプログラミングがくしゅう".include?("WEBCAMP")
---
+++ Ruby/lesson5.rb
@@ -0,0 +1,11 @@
+total_price = 120
+
+if total_price > 100
+  puts "みかんを購入。所持金に余りあり。"
+
+elsif total_price == 100
+  puts "みかんを購入。所持金は０円"
+
+else
+  puts "みかんを購入することが出来ません。"
+end
